d6572a917b066d02586bd8d5a2a7c1b2ca1eb619
jaenwawe <nwawe.jae@gmail.com>
2021-07-28 00:43:22 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function countDown(count){
+    while (count >= 0 ) {
+    console.log(count--);
+    } 
+}
+
+
+function writeCards(name, event){
+    let copyNames = [];
+    for (let count = 0; count < name.length; count++) {
+      copyNames.push(`Thank you, ${name[count]}, for the wonderful ${event} gift!`);
+      } 
+return copyNames;
+}
